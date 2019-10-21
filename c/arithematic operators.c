140c20b1ca3e84ed66a849d4a0820d3c65cac73a
jyotisingla8611 <37078263+jyotisingla8611@users.noreply.github.com>
2019-10-21 23:51:27 +0530

    Update arithematic operators.c

--- c/arithematic operators.c
+++ c/arithematic operators.c
@@ -7,14 +7,14 @@
         scanf("%d %d",&num1,&num2);
         printf("Various Arithematic Operations on the numbers are :\n");
         res=num1+num2;
-        printf("%d + %d = %d\n",num1,num2,res);
+        printf("Sum is %d + %d = %d\n",num1,num2,res);
         res=num1-num2;
-        printf("%d - %d = %d\n",num1,num2,res);
+        printf("Subtraction is %d - %d = %d\n",num1,num2,res);
         res=num1*num2;
-        printf("%d * %d = %d\n",num1,num2,res);
+        printf("Multiplication is %d * %d = %d\n",num1,num2,res);
         res=num1/num2;
-        printf("%d / %d = %d\n",num1,num2,res);
+        printf("Division is %d / %d = %d\n",num1,num2,res);
         res=num1%num2;
-        printf("%d %% %d = %d\n",num1,num2,res);
+        printf("Mod is %d %% %d = %d\n",num1,num2,res);
         return 0;
     }
